30d0a75e053dd1c329f36f6a3e869d6b9b48ca97
m.shintaro95 <mitome.shintaro448@mail.kyutech.jp>
2026-04-03 23:55:17 +0900

    7P1　修正1

--- Tex/RegressionResultPrinter.java
+++ Tex/RegressionResultPrinter.java
@@ -1,15 +1,22 @@
 public class RegressionResultPrinter {
     public static void main(String[] args) {
         System.out.println(
-            "rxy = 0.55902\n" +
-            "r^2 = 0.3125\n" +
-            "tail area = 19.2\n" +
-            "B0 = -141.1716\n" +
-            "B1 = 7.0559\n" +
-            "yk = 194.69\n" +
-            "Range = 240.66\n" +
-            "UPI = 435.35\n" +
-            "LPI = -45.97"
+            "rxy = 0.933306898\n" +
+            "r^2 = 0.871061766\n" +
+            "tail area = 7.98203e-05\n" +
+            "B0 = -4.038881575\n" +
+            "B1 = 0.16812665\n" +
+            "yk = 60.85800528\n" +
+            "Range = 27.55764748\n" +
+            "UPI = 88.41565276\n" +
+            "LPI = 33.3003578"
         );
     }
 }
+
+
+/*
+    String s = String.format("%.10g", v);
+    // BigDecimalを経由してゼロを除去
+    System.out.println(new BigDecimal(s).stripTrailingZeros().toPlainString());
+ */
